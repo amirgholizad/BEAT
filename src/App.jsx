cacbb3a94a724e74a36a7dd52147756e4e3d6b60
amirg <agholizadgha@mun.ca>
2025-02-13 15:00:09 -0330

    login and signup page complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,89 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 import Home from "./pages/Home/Home";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import "./App.scss";
 
 function App() {
-  const [authorised, setAuthorised] = useState(false);
+  let accessToken = localStorage.getItem("token");
+  const [authorized, setAuthorized] = useState(false);
+
+  async function isAuthorized(accessToken) {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_URL}/user/authorized`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        setAuthorized(true);
+        return response;
+      } else {
+        setAuthorized(false);
+        return response;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useState(() => {
+    if (accessToken) {
+      console.log(isAuthorized(accessToken));
+    }
+  }, []);
+
+  if (accessToken) {
+    setTimeout(() => {
+      localStorage.removeItem("token");
+      window.location.reload();
+      setAuthorized(false);
+      accessToken = null;
+      1;
+    }, 1000 * 60);
+  }
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/login" element={<LoginPage path="login" />} />
-          <Route path="/signup" element={<LoginPage path="signup" />} />
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={
+              !authorized ? (
+                <LoginPage path="login" />
+              ) : (
+                <Navigate replace to="/" />
+              )
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              !authorized ? (
+                <LoginPage path="signup" />
+              ) : (
+                <Navigate replace to="/" />
+              )
+            }
+          />
+          <Route
+            path="/"
+            element={authorized ? <Home /> : <Navigate replace to="/login" />}
+          />
         </Routes>
       </Router>
     </>
   );
 }
-
 export default App;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,6 @@ import { useNavigate } from "react-router";
 import { useEffect } from "react";
 
 function Home() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log(localStorage.getItem("token"));
-    if (localStorage.getItem("token") === null) {
-      navigate("/login");
-    }
-  }, [navigate]);
   return (
     <div>
       <h1>Home</h1>
